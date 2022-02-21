eaf1720ea3f8613aac48d81946cd49328da421bb
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-21 14:10:37 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioQuatroListaTres/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioQuatroListaTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada!");
+
+            Console.Write("Digite um valor inteiro de 1 a 10: ");
+            string strValor = Console.ReadLine();
+            int valor = Convert.ToInt32(strValor);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", i, valor, i * valor);
+            }
+        }
+    }
+}
